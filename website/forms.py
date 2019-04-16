9b901f0863e95e116b755ed4180d4227f5de887c
psyduck31 <47318921+psyduck31@users.noreply.github.com>
2019-04-16 13:29:07 +0300

    Add files via upload

--- website/forms.py
+++ website/forms.py
@@ -4,7 +4,6 @@ from wtforms.validators import DataRequired, Length
 
 
 class Add(FlaskForm):
-    author = StringField('Автор', validators=[DataRequired(), Length(min=4, max=20)])
     title = StringField('Заголовок', validators=[DataRequired(), Length(min=10, max=35)])
     text = StringField('Напишите что-нибудь...', validators=[DataRequired(), Length(min=60, max=255)])
     submit = SubmitField('Создать')
