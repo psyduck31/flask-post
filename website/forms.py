f70e0c13fe047918fdd3fcaa52018d58908ae436
psyduck31 <47318921+psyduck31@users.noreply.github.com>
2019-04-16 10:37:02 +0300

    Add files via upload

--- website/forms.py
+++ website/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Length
 
 
@@ -8,3 +8,9 @@ class Add(FlaskForm):
     title = StringField('Заголовок', validators=[DataRequired(), Length(min=10, max=35)])
     text = StringField('Напишите что-нибудь...', validators=[DataRequired(), Length(min=60, max=255)])
     submit = SubmitField('Создать')
+
+
+class Login(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
--- website/routes.py
+++ website/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, url_for
-from website import app, db
-from website.forms import Add
+from website import app, db, login_manager
+from website.forms import Add, Login
+from website.models import User, Posts
+from flask_login import login_user, current_user, logout_user, login_required
 import sqlite3
 
 
@@ -12,6 +14,21 @@ def home():
 @app.route('/post/<int:post_id>')
 def post(post_id):
     return str(post_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = Login()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user.password == form.password.data:
+            login_user(user, remember=True)
+    return render_template('login.html', form=form)
+
+
+@app.route('/test')
+def test():
+    return str(current_user.username)
 
 """
 @app.route('/post/add')
